c41703f57a58cf8d1f4778aa9d220e3ef1744cb1
edmaputra <bangun.edma.saputra@gmail.com>
2016-07-26 17:29:47 +0800

    no improvement

--- src/main/java/org/bankaltim/edmaputra/bpdtjstaskscheduler/NewMainFrame.java
+++ src/main/java/org/bankaltim/edmaputra/bpdtjstaskscheduler/NewMainFrame.java
@@ -21,7 +21,7 @@ public class NewMainFrame extends JFrame {
 	private static final long serialVersionUID = 2534689276808583670L;
 
 	private Scheduler scheduler;
-	
+
 	private JobKey jobKey1;
 	private JobKey jobKey2;
 	private JobKey jobKey3;
@@ -79,16 +79,21 @@ public class NewMainFrame extends JFrame {
 				int errorCode = validasi();
 				if (errorCode == 0) {
 					int errorCode1 = validasiPerTask();
+					validateWaktu(mainPanel1.getTxtJam(), mainPanel1.getTxtMenit());
 					if (errorCode1 == 0) {
 						startScheduler();
 						startButtonClicked();
 					} else if (errorCode == 2) {
-						JOptionPane.showMessageDialog(null, "Harap Isi Waktu dengan Benar", "Isi Waktu",
+						JOptionPane.showMessageDialog(null,
+								"Harap Isi Waktu dengan Benar", "Isi Waktu",
 								JOptionPane.WARNING_MESSAGE);
 					}
 				} else if (errorCode == 1) {
-					JOptionPane.showMessageDialog(null, "Harap Isi Salah Satu Task dengan waktu yang ingin ditentukan",
-							"Isi Waktu", JOptionPane.WARNING_MESSAGE);
+					JOptionPane
+							.showMessageDialog(
+									null,
+									"Harap Isi Salah Satu Task dengan waktu yang ingin ditentukan",
+									"Isi Waktu", JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		});
@@ -96,7 +101,7 @@ public class NewMainFrame extends JFrame {
 		btnStopAll.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				try {
-					// scheduler.shutdown();
+					scheduler.clear();
 					stopButtonClicked();
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
@@ -116,51 +121,63 @@ public class NewMainFrame extends JFrame {
 			if (!mainPanel1.getTxtJam().getText().isEmpty()) {
 				jobKey1 = new JobKey(mainPanel1.getJobName());
 				JobDetail job1 = setDoaPagiJob(jobKey1);
-				Trigger trigger1 = setTrigger(job1, mainPanel1.getTxtJam().getText(),
-						mainPanel1.getTxtMenit().getText(), mainPanel1.getTriggerName(), mainPanel1.getTriggerGroup());
+				Trigger trigger1 = setTrigger(job1, mainPanel1.getTxtJam()
+						.getText(), mainPanel1.getTxtMenit().getText(),
+						mainPanel1.getTriggerName(),
+						mainPanel1.getTriggerGroup());
 				scheduler.scheduleJob(job1, trigger1);
 			}
-			
+
 			if (!mainPanel2.getTxtJam().getText().isEmpty()) {
 				jobKey2 = new JobKey(mainPanel2.getJobName());
 				JobDetail job2 = setCorporateSongJob(jobKey2);
-				Trigger trigger2 = setTrigger(job2, mainPanel2.getTxtJam().getText(), mainPanel2.getTxtMenit().getText(),
-						mainPanel2.getTriggerName(), mainPanel2.getTriggerGroup());
+				Trigger trigger2 = setTrigger(job2, mainPanel2.getTxtJam()
+						.getText(), mainPanel2.getTxtMenit().getText(),
+						mainPanel2.getTriggerName(),
+						mainPanel2.getTriggerGroup());
 				scheduler.scheduleJob(job2, trigger2);
 			}
-			
+
 			if (!mainPanel3.getTxtJam().getText().isEmpty()) {
 				jobKey3 = new JobKey(mainPanel3.getJobName());
 				JobDetail job3 = setCorporateSongJob(jobKey3);
-				Trigger trigger3 = setTrigger(job3, mainPanel3.getTxtJam().getText(), mainPanel3.getTxtMenit().getText(),
-						mainPanel3.getTriggerName(), mainPanel3.getTriggerGroup());
+				Trigger trigger3 = setTrigger(job3, mainPanel3.getTxtJam()
+						.getText(), mainPanel3.getTxtMenit().getText(),
+						mainPanel3.getTriggerName(),
+						mainPanel3.getTriggerGroup());
 				scheduler.scheduleJob(job3, trigger3);
 			}
-			
+
 			if (!mainPanel4.getTxtJam().getText().isEmpty()) {
 				jobKey4 = new JobKey(mainPanel4.getJobName());
 				JobDetail job4 = setMarsBankaltimJob(jobKey4);
-				Trigger trigger4 = setTrigger(job4, mainPanel4.getTxtJam().getText(), mainPanel4.getTxtMenit().getText(),
-						mainPanel4.getTriggerName(), mainPanel4.getTriggerGroup());
+				Trigger trigger4 = setTrigger(job4, mainPanel4.getTxtJam()
+						.getText(), mainPanel4.getTxtMenit().getText(),
+						mainPanel4.getTriggerName(),
+						mainPanel4.getTriggerGroup());
 				scheduler.scheduleJob(job4, trigger4);
 			}
-			
+
 			if (!mainPanel5.getTxtJam().getText().isEmpty()) {
 				jobKey5 = new JobKey(mainPanel5.getJobName());
 				JobDetail job5 = setAdzanJob(jobKey5);
-				Trigger trigger5 = setTrigger(job5, mainPanel5.getTxtJam().getText(), mainPanel5.getTxtMenit().getText(),
-						mainPanel5.getTriggerName(), mainPanel5.getTriggerGroup());
+				Trigger trigger5 = setTrigger(job5, mainPanel5.getTxtJam()
+						.getText(), mainPanel5.getTxtMenit().getText(),
+						mainPanel5.getTriggerName(),
+						mainPanel5.getTriggerGroup());
 				scheduler.scheduleJob(job5, trigger5);
 			}
-			
+
 			if (!mainPanel6.getTxtJam().getText().isEmpty()) {
 				jobKey6 = new JobKey(mainPanel6.getJobName());
 				JobDetail job6 = setAdzanJob(jobKey6);
-				Trigger trigger6 = setTrigger(job6, mainPanel6.getTxtJam().getText(), mainPanel6.getTxtMenit().getText(),
-						mainPanel6.getTriggerName(), mainPanel6.getTriggerGroup());
+				Trigger trigger6 = setTrigger(job6, mainPanel6.getTxtJam()
+						.getText(), mainPanel6.getTxtMenit().getText(),
+						mainPanel6.getTriggerName(),
+						mainPanel6.getTriggerGroup());
 				scheduler.scheduleJob(job6, trigger6);
-			}		
-			
+			}
+
 			scheduler.start();
 		} catch (SchedulerException e) {
 			// TODO Auto-generated catch block
@@ -208,12 +225,14 @@ public class NewMainFrame extends JFrame {
 	}
 
 	private JobDetail setMarsBankaltimJob(JobKey jobKey) {
-		JobDetail job = newJob(MarsBankaltimJob.class).withIdentity(jobKey).build();
+		JobDetail job = newJob(MarsBankaltimJob.class).withIdentity(jobKey)
+				.build();
 		return job;
 	}
 
 	private JobDetail setCorporateSongJob(JobKey jobKey) {
-		JobDetail job = newJob(CorporateSongJob.class).withIdentity(jobKey).build();
+		JobDetail job = newJob(CorporateSongJob.class).withIdentity(jobKey)
+				.build();
 		return job;
 	}
 
@@ -227,9 +246,11 @@ public class NewMainFrame extends JFrame {
 		scheduler.clear();
 	}
 
-	private Trigger setTrigger(JobDetail job, String hour, String minute, String triggerName, String groupName) {
+	private Trigger setTrigger(JobDetail job, String hour, String minute,
+			String triggerName, String groupName) {
 		Trigger trigger = newTrigger().withIdentity(triggerName, groupName)
-				.withSchedule(cronSchedule(setCronExpression(hour, minute))).forJob(job).build();
+				.withSchedule(cronSchedule(setCronExpression(hour, minute)))
+				.forJob(job).build();
 		return trigger;
 	}
 
@@ -257,9 +278,10 @@ public class NewMainFrame extends JFrame {
 		String menit5 = mainPanel5.getTxtMenit().getText();
 		String menit6 = mainPanel6.getTxtMenit().getText();
 
-		if (jam1.isEmpty() && jam2.isEmpty() && jam3.isEmpty() && jam4.isEmpty() && jam5.isEmpty() && jam6.isEmpty()
-				&& menit1.isEmpty() && menit2.isEmpty() && menit3.isEmpty() && menit4.isEmpty() && menit5.isEmpty()
-				&& menit6.isEmpty()) {
+		if (jam1.isEmpty() && jam2.isEmpty() && jam3.isEmpty()
+				&& jam4.isEmpty() && jam5.isEmpty() && jam6.isEmpty()
+				&& menit1.isEmpty() && menit2.isEmpty() && menit3.isEmpty()
+				&& menit4.isEmpty() && menit5.isEmpty() && menit6.isEmpty()) {
 			errorCode = 1;
 		}
 
@@ -268,69 +290,94 @@ public class NewMainFrame extends JFrame {
 
 	private int validasiPerTask() {
 		int errorCode = 0;
-		
-		if (!mainPanel1.getTxtJam().getText().isEmpty()) {
-			if (validateWaktu(mainPanel1.getTxtJam(), mainPanel1.getTxtMenit()) == false) {
-				errorCode = 2;
+
+		if (!mainPanel1.getTxtJam().getText().isEmpty()
+				&& !mainPanel1.getTxtMenit().getText().isEmpty()) {
+			
+			if (mainPanel1.getTxtJam().getText().trim().isEmpty()) {
+				mainPanel1.getTxtJam().setText("00");
 			}
+
+			if (mainPanel1.getTxtMenit().getText().trim().isEmpty()) {
+				mainPanel1.getTxtMenit().setText("00");
+			}
+			// errorCode = validateWaktu(mainPanel1.getTxtJam(),
+			// mainPanel1.getTxtMenit());
 		}
 
-		if (!mainPanel2.getTxtJam().getText().isEmpty()) {
-			if (validateWaktu(mainPanel2.getTxtJam(), mainPanel2.getTxtMenit()) == false) {
-				errorCode = 2;
-			}
-		}
-
-		if (!mainPanel3.getTxtJam().getText().isEmpty()) {
-			if (validateWaktu(mainPanel3.getTxtJam(), mainPanel3.getTxtMenit()) == false) {
-				errorCode = 2;
-			}
-		}
-
-		if (!mainPanel4.getTxtJam().getText().isEmpty()) {
-			if (validateWaktu(mainPanel4.getTxtJam(), mainPanel4.getTxtMenit()) == false) {
-				errorCode = 2;
-			}
-		}
-
-		if (!mainPanel5.getTxtJam().getText().isEmpty()) {
-			if (validateWaktu(mainPanel5.getTxtJam(), mainPanel5.getTxtMenit()) == false) {
-				errorCode = 2;
-			}
-		}
-
-		if (!mainPanel6.getTxtJam().getText().isEmpty()) {
-			if (validateWaktu(mainPanel6.getTxtJam(), mainPanel6.getTxtMenit()) == false) {
-				errorCode = 2;
-			}
-		}
+		// if (!mainPanel1.getTxtJam().getText().isEmpty()) {
+		// if (validateWaktu(mainPanel1.getTxtJam(), mainPanel1.getTxtMenit())
+		// == false) {
+		// errorCode = 2;
+		// }
+		// }
+		//
+		// if (!mainPanel2.getTxtJam().getText().isEmpty()) {
+		// if (validateWaktu(mainPanel2.getTxtJam(), mainPanel2.getTxtMenit())
+		// == false) {
+		// errorCode = 2;
+		// }
+		// }
+		//
+		// if (!mainPanel3.getTxtJam().getText().isEmpty()) {
+		// if (validateWaktu(mainPanel3.getTxtJam(), mainPanel3.getTxtMenit())
+		// == false) {
+		// errorCode = 2;
+		// }
+		// }
+		//
+		// if (!mainPanel4.getTxtJam().getText().isEmpty()) {
+		// if (validateWaktu(mainPanel4.getTxtJam(), mainPanel4.getTxtMenit())
+		// == false) {
+		// errorCode = 2;
+		// }
+		// }
+		//
+		// if (!mainPanel5.getTxtJam().getText().isEmpty()) {
+		// if (validateWaktu(mainPanel5.getTxtJam(), mainPanel5.getTxtMenit())
+		// == false) {
+		// errorCode = 2;
+		// }
+		// }
+		//
+		// if (!mainPanel6.getTxtJam().getText().isEmpty()) {
+		// if (validateWaktu(mainPanel6.getTxtJam(), mainPanel6.getTxtMenit())
+		// == false) {
+		// errorCode = 2;
+		// }
+		// }
 
 		return errorCode;
 	}
 
-	private Boolean validateWaktu(JTextField jam, JTextField menit) {
-		Boolean validated = true;
+	private int validateWaktu(JTextField jam, JTextField menit) {
+		if (!jam.getText().isEmpty() && !menit.getText().isEmpty()){
+			int errorCode = 0;
+			System.out.println("ABC");
 
-		if (jam.getText().trim().isEmpty()) {
-			jam.setText("00");
+			if (jam.getText().trim().isEmpty()) {
+				jam.setText("00");
+			}
+
+			if (menit.getText().trim().isEmpty()) {
+				menit.setText("00");
+			}
+
+			Integer jamInt = Integer.parseInt(jam.getText());
+			Integer menitInt = Integer.parseInt(menit.getText());
+
+			if (jamInt < 0 || jamInt > 24) {
+				errorCode = 2;
+			}
+
+			if (menitInt < 0 || menitInt > 59) {
+				errorCode = 2;
+			}
+
+			return errorCode;
+		}else {
+			return -1;
 		}
-
-		if (menit.getText().trim().isEmpty()) {
-			menit.setText("00");
-		}
-
-		Integer jamInt = Integer.parseInt(jam.getText());
-		Integer menitInt = Integer.parseInt(menit.getText());
-
-		if (jamInt < 0 || jamInt > 24) {
-			validated = false;
-		}
-
-		if (menitInt < 0 || menitInt > 59) {
-			validated = false;
-		}
-
-		return validated;
 	}
 
 	private MainPanel mainPanel1;
